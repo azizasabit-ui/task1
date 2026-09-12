b3dcd6d8d4939ea4690f65732c3fa5a517edb081
azizasabit-ui <aziza.sabi@narxoz.kz>
2026-09-12 15:15:40 +0500

    Add multiplication table

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,66 @@
-import 'package:flutter/material.dart';
-
 void main() {
-  runApp(const MyApp());
-}
+  String name = "Bekzat";
+  int age = 25;
+  double gpa = 3.4;
+  bool isStudent = false;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  print("name : $name\nage: $age y.o.\ngpa: $gpa\nis Teacher: ${!isStudent}");
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  String text1 = "Hello";
+  // String nullText = null; not works
+  String? text2 = null;
+  print('text1: $text1');
+  print('text2: $text2');
+
+  int length1 = text1.length;
+  int length2 = text2?.length ?? 0;
+  print(length1);
+  print(length2);
+
+  String confirmedText = text2 ?? "default";
+  print("confirmed $confirmedText length: ${confirmedText.length}");
+
+  int digit = 3;
+  print("MULTIPLICATION TABLE for digit $digit");
+  for (int i = 1; i <= 10; i++) {
+    print("$digit * $i = ${i * digit}");
+  }
+
+  for (int i = 10; i > 0; i--) {
+    if (i % 2 == 0) {
+      print("$i");
+    } else {
+      print("${i * 2}");
+    }
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// TASK 1
+// OUTPUT MULTIPLICATION TABLE  1-10
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+// TASK 2
+// next day : examples:
+// 05.09.2026 -> 06.09.2026
+// 28.02.2024 -> 29.02.2024
+// 28.02.2026 -> 01.03.2026
+// 29.02.2026 -> invalid date
+// 28.02.2100 -> 01.03.2100
+// 28.02.2100 -> 29.02.2000
+// 31.12.2025 -> 01.01.2026
+// 2000,2400 leap year
+// 2100,2200,2300 isn`t leap year
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+// TASK3
+// Vowel Counter in a String -> "flutter mobile development" -> 8
 
-  final String title;
+List<int> numbers = [14, 88, 3, 42, 99, 12, 67]; //-> max: 99, min: 3
+List<int> numbers1 = [234, 34, 123, 44, 949, 112, 67]; //-> max: 949, min: 34
+int first = numbers[0];
+int last = numbers[numbers.length - 1];
+// TASK4
+// Manual min & max finder
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
+// TASK 5
+// Prime Number Checker
+// 3 -> prime number
+// 6 -> not prime number
